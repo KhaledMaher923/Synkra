d6193ae64833006bc991a2dbaf164118f25d429a
SohaibAlaraby <sohibalaraby9@gmail.com>
2026-08-29 19:14:56 +0300

    chore: install and verify react-icons package

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-
+import { FaRocket } from 'react-icons/fa';
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold text-red-600'>Hello World</h1>
+      <h1 className='text-3xl font-bold text-red-600 inline'>Hello World</h1>
+      <FaRocket className='text-3xl text-blue-700 inline'/>
     </>
   )
 }
