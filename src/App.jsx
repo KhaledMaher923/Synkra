d3a416dc68d18a3f79413970b4182d1420518816
SohaibAlaraby <sohibalaraby9@gmail.com>
2026-08-29 20:21:51 +0300

    chore: add and verify the necessary fonts like Newsreader and DM Sans from Google Fonts

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold text-red-600 inline'>Hello World</h1>
+      <h1 className='text-3xl text-red-600 inline'>Hello World</h1>
+      <p className='text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum voluptatem quaerat libero eos quod nesciunt veritatis consequuntur recusandae, debitis, repudiandae cum. Id dolorem nisi sed, blanditiis optio praesentium? Repellat, alias.</p>
       <FaRocket className='text-3xl text-blue-700 inline'/>
     </>
   )
