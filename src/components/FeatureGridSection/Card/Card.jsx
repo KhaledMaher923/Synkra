dfb9e28e406583e44b539b538cf4a1a8ab216326
Amira-Elsayed-Abdelaziz <amira.elsayed.abdelaziz@gmail.com>
2026-09-02 20:27:03 +0300

    adding Card component

---
+++ src/components/FeatureGridSection/Card/Card.jsx
@@ -0,0 +1,13 @@
+import CardBody from "./CardBody";
+import CardImage from "./CardImage";
+
+export default function Card({theme,info} )
+{
+    const classes = info.id=='recovery'?'bg-[#1A56DB] text-[#FCFCFD] ' :  (info.id=='visual-builder' ? 'bg-[#F4A016]' : 'bg-[#FCFCFD]')
+    return (
+        <div className={`border-1 border-[#E4E3DF] rounded-[12px] p-[16px] + ${classes}`}>
+<CardBody id={info.id} badge={info.badge} title={info.title} content={info.content} img={info.img} imgTop={info.imgTop} footer={info.footer}/>
+
+        </div>
+    )
+}
