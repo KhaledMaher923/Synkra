93da035d7e27f35283ef3694aee05fb041a041c3
Amira-Elsayed-Abdelaziz <amira.elsayed.abdelaziz@gmail.com>
2026-09-04 06:00:33 +0300

    Fixing badges style issue

--- src/components/FeatureGridSection/Card/CardBadge.jsx
+++ src/components/FeatureGridSection/Card/CardBadge.jsx
@@ -2,9 +2,15 @@ import { useTheme } from "../../../context/ThemeContext.jsx";
 
 export default function CardBadge({ badge }) {
   const { theme } = useTheme();
+const activeBg = theme == "dark"  ? badge.bgColorDark : badge.bgColor;
+  const activeText = theme == "dark"  ? badge.textColorDark : badge.textColor;
   return (
     <div
-      className={`text-[12px] flex gap-[5px] pt-[4px] pb-[4px] pr-[12px] pl-[12px] md:pt-[8px] md:pb-[8px] md:pr-[12px] md:pl-[12px] rounded-[999px] self-start ${theme == "dark" ? `bg-[${badge.bgColorDark}] text-[${badge.textColorDark}]` : `bg-[${badge.bgColor}] text-[${badge.textColor}]`}`}
+      className={`text-[12px] flex gap-[5px] pt-[4px] pb-[4px] pr-[12px] pl-[12px] md:pt-[8px] md:pb-[8px] md:pr-[12px] md:pl-[12px] rounded-[999px] self-start`}
+    style={{
+        backgroundColor: activeBg,
+        color: activeText,
+      }}
     >
       <img src={badge.icon} />
       <span>{badge.text}</span>
