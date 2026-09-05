b914b8329ebcfac69c38a612fcdd94633b580593
Amira-Elsayed-Abdelaziz <amira.elsayed.abdelaziz@gmail.com>
2026-09-05 20:39:16 +0300

    modifiying icons in the Feature Grid Section components

--- src/components/FeatureGridSection/Card/CardBadge.jsx
+++ src/components/FeatureGridSection/Card/CardBadge.jsx
@@ -6,13 +6,13 @@ const activeBg = theme == "dark"  ? badge.bgColorDark : badge.bgColor;
   const activeText = theme == "dark"  ? badge.textColorDark : badge.textColor;
   return (
     <div
-      className={`text-[12px] flex gap-[5px] pt-[4px] pb-[4px] pr-[12px] pl-[12px] md:pt-[8px] md:pb-[8px] md:pr-[12px] md:pl-[12px] rounded-[999px] self-start`}
+      className={`text-[12px] flex items-center gap-[5px] pt-[4px] pb-[4px] pr-[12px] pl-[12px] md:pt-[8px] md:pb-[8px] md:pr-[12px] md:pl-[12px] rounded-[999px] self-start`}
     style={{
         backgroundColor: activeBg,
         color: activeText,
       }}
     >
-      <img src={badge.icon} />
+      {badge.icon}
       <span>{badge.text}</span>
     </div>
   );
--- src/components/FeatureGridSection/FeatureGridSectionHeader.jsx
+++ src/components/FeatureGridSection/FeatureGridSectionHeader.jsx
@@ -14,13 +14,14 @@ export default function FeatureGridSectionHeader({
         <div className="bg-[#1A56DB] w-[128px] h-[4px]"></div>
       </h2>
       <div className="md:w-[600px] gap-[12px] flex flex-col">
-        <span
-          className={`font-sans text-[12px]
+        <div
+          className={`flex items-center gap-[5px] font-sans text-[12px]
             
             ${theme === "dark" ? "text-[#75A2F0] bg-[#081D5B]" : "text-primary-blue bg-[#EBF2FD] "} pt-[4px] pb-[4px] pr-[12px] pl-[12px] self-start rounded-[999px]`}
         >
-          <img className="inline" src={icon} /> {badge}
-        </span>
+          {icon}
+          <span>{badge}</span>
+        </div>
         <p
           className={`font-sans text-[14px] ${theme === "dark" ? "text-[#CBC9C2]" : "text-medium-gray"}`}
         >
