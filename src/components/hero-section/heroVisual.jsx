b2af8a9cf16d2f6c42fdd2048d9e7d2fb8c68b55
EsraaMousa18 <esra1ali188@gmail.com>
2026-09-06 05:39:24 +0300

    Image positions

--- src/components/hero-section/heroVisual.jsx
+++ src/components/hero-section/heroVisual.jsx
@@ -8,27 +8,28 @@ function HeroVisual() {
   const { theme } = useTheme();
 
   return (
-    <div className="relative max-w-md mb-12, hidden md:block">
+    <div className="relative md:w-[580px] md:h-[600px] mb-12 hidden md:block overflow-visible">
       <img
         src={buildingImg}
         alt="Enterprise building"
-        className="w-full rounded-2xl object-cover"
+        className="w-full h-full rounded-2xl object-cover"
       />
- <div className="absolute top-6 right-6 w-[180px] bg-[#FAFAFB]/15 backdrop-blur-md rounded-xl p-4 flex flex-col gap-3 shadow-[0_2px_10px_1px_#02061719] border border-white/20">
-  <div className="flex items-center gap-2">
-    <span className="bg-straight-green/10 text-straight-green rounded-full p-1.5">
-      <HiOutlineChartBar className="w-4 h-4" />
-    </span>
-    <span className="text-xs font-bold text-dark-theme">
-      {visual.efficiency.label}
-    </span>
-  </div>
-  <p className="text-3xl font-header font-extrabold text-straight-green leading-[120%] ">{visual.efficiency.value}</p>
-  <p className="text-xs text-black">{visual.efficiency.caption}</p>
-</div>
 
-        <div className="absolute -bottom-6 left-4 w-[508px] max-w-[calc(100%-2rem)] bg-black/40 backdrop-blur-md rounded-xl p-4 flex flex-col gap-3 border border-white/10">
-            <div className="flex items-center justify-between">
+      <div className="absolute top-6 right-6 w-[180px] bg-[#FAFAFB]/15 backdrop-blur-md rounded-xl p-4 flex flex-col gap-3 shadow-[0_2px_10px_1px_#02061719] border border-white/20">
+        <div className="flex items-center gap-2">
+          <span className="bg-straight-green/10 text-straight-green rounded-full p-1.5">
+            <HiOutlineChartBar className="w-4 h-4" />
+          </span>
+          <span className="text-xs font-bold text-dark-theme">
+            {visual.efficiency.label}
+          </span>
+        </div>
+        <p className="text-3xl font-header font-extrabold text-straight-green leading-[120%]">{visual.efficiency.value}</p>
+        <p className="text-xs text-black">{visual.efficiency.caption}</p>
+      </div>
+
+      <div className="absolute -bottom-1 left-1/2 -translate-x-1/2 w-[680px] bg-black/40 backdrop-blur-md rounded-xl p-4 flex flex-col gap-3 border border-white/10 z-20 shadow-2xl">
+        <div className="flex items-center justify-between">
           <div className="flex items-center gap-2">
             <span className="bg-straight-green rounded-full p-1">
               <HiCheck className="w-3 h-3 text-white" />
@@ -44,7 +45,6 @@ function HeroVisual() {
             style={{ width: visual.sync.percent }}
           ></div>
         </div>
-
 
         <p className="text-single-orange text-xs italic font-sans-serif">{visual.footer}</p>
       </div>
