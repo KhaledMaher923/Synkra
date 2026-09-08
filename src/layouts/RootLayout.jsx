6580c89236e2b49d860669eb5e0f5ce1b7a36db2
SohaibAlaraby <sohibalaraby9@gmail.com>
2026-09-08 15:00:48 +0300

    feat(route): create root layout for pages routing

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import TopNavigation from "../components/Navigation/TopNavigation";
+
+export function RootLayout(){
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    return (
+        <div className="flex flex-col min-h-screen">
+            <TopNavigation onOpenMobileMenu={() => setIsMobileMenuOpen(true)} />
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
+}
