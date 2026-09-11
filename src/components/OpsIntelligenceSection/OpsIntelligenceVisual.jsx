1613c384fb3f4eb9f0e88ca92fbb42667d92f699
khaled <khaledmaher923@gmail.com>
2026-09-12 00:44:53 +0300

    Refactor OpsIntelligenceVisual: adjust layout, update image class, and enhance styling

--- src/components/OpsIntelligenceSection/OpsIntelligenceVisual.jsx
+++ src/components/OpsIntelligenceSection/OpsIntelligenceVisual.jsx
@@ -3,15 +3,15 @@ import { OISQuote } from "../../data/opsIntelligenceSectionData.jsx";
 
 export default function OpsIntelligenceVisual() {
     return (
-        <div className="relative w-full lg:w-1/2 pb-10 pl-4 lg:pb-14 lg:pl-10" >
+        <div className="relative w-full h-[388px] lg:w-1/2  lg:pb-14 lg:pl-10" >
             <img
                 src={intelligenceImg}
                 alt="Synkra logic engine running on a developer's workstation"
-                className="w-[518px] h-[388px] max-h-125 object-cover  object-top rounded-2xl"
+                className="w-[508px] h-[388px] max-h-125 object-cover object-[0px_-20px] overflow-hidden rounded-2xl"
             />
 
-            <div className="absolute bottom-[18px] left-[10px] w-[calc(100%-2.5rem)] sm:w-90 p-6 rounded-2xl bg-black/35 backdrop-blur-[2px] backdrop-saturate-150 border border-white/15 shadow-[0_8px_32px_rgba(29, 27, 25, 0.25)] " >
-                <p className="font-mono text-xs font-bold italic text-semi-white/70 mb-3" >
+            <div className="absolute bottom-[-28px] left-[10px] w-[calc(100%-2.5rem)] lg:w-90 p-6 rounded-2xl bg-black/55 backdrop-blur-[2px] backdrop-saturate-150 border border-white/15 " >
+                <p className="font-mono text-xs font-bold italic text-semi-white mb-3" >
                     {OISQuote.tag}
                 </p>
                 <p className="font-sans text-[14] text-base text-semi-white leading-relaxed">
