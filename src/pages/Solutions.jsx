721b8cab83a79739be208aaeb402ce83aa0407df
Amira-Elsayed-Abdelaziz <amira.elsayed.abdelaziz@gmail.com>
2026-09-13 16:07:36 +0300

    removing the old CTA component

--- src/pages/Solutions.jsx
+++ src/pages/Solutions.jsx
@@ -16,7 +16,6 @@ export function Solutions(){
             <StatsSection/>
             <TestimonialsSection/>
             <FAQSection/>
-            <LandingCtaSection/>
             <CTASection/>
         </>
     );
