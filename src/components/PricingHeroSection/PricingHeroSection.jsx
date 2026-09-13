8b49814f46bde831529e1a9579c81010ec5316bc
SohaibAlaraby <sohibalaraby9@gmail.com>
2026-09-13 13:56:37 +0300

    feat(pricing): add dark and light themes to the pricing hero section

--- src/components/PricingHeroSection/PricingHeroSection.jsx
+++ src/components/PricingHeroSection/PricingHeroSection.jsx
@@ -1,6 +1,6 @@
 import {useState,useMemo} from 'react';
 import {Switch} from './Switch';
-
+import { useTheme } from '../../context/ThemeContext';
 function splitTitle(title){
     if(!title) return ['',''];
     const sentences = title.split('.');
@@ -14,30 +14,31 @@ function splitTitle(title){
 export function PricingHeroSection({title='',subtitle='',pageLabel='',paymentOptions=[], icons={}}){
     const [isAnnual, setIsAnnual] = useState(false);
     const [firstWord, restOfFirstSentence, secondSentence] = useMemo(() => splitTitle(title),[title]);
+    const {theme} = useTheme();
     const PriceIcon = icons?.PriceIcon;
     const CircleCheckIcon = icons?.CircleCheckIcon;
     const ArrowIcon = icons?.ArrowIcon;
 
 
     return (
-        <section className='p-16 bg-light-theme'>
+        <section className={`p-16 ${theme==='dark'?'bg-dark-theme':'bg-light-theme'}`}>
             <header className='flex flex-col items-center gap-3'>
-                <div className='uppercase flex items-center gap-1 py-1 px-3 text-primary-blue bg-[#EBF2FD] rounded-full text-[12px]'>
+                <div className={`uppercase flex items-center gap-1 py-1 px-3  rounded-full text-[12px] ${theme === 'dark'?'bg-[#081D5B] text-[#75A2F0]':'bg-[#EBF2FD] text-primary-blue'}`}>
                     {PriceIcon && <PriceIcon className='scale-x-[-1]'/>}
                     {pageLabel}
                 </div>
-                <h2 className='font-normal text-[80px] flex flex-col items-center font-header'><span><span className='text-primary-blue italic'>{firstWord}</span> {restOfFirstSentence}.</span><span>{secondSentence}.</span></h2>
-                <p className='text-[20px] text-medium-gray font-bold text-center'>{subtitle}</p>
+                <h2 className={`font-normal text-[80px] flex flex-col items-center font-header ${theme === 'dark'?' text-white':'text-dark-theme'}`}><span><span className={`italic ${theme === 'dark'?' text-[#75A2F0]':'text-primary-blue'}`}>{firstWord}</span> {restOfFirstSentence}.</span><span>{secondSentence}.</span></h2>
+                <p className={`text-[20px] ${theme ==='dark'?'text-[#CBC9C2]':'text-medium-gray'} font-bold text-center`}>{subtitle}</p>
             </header>
             <div>
                 <div className='flex gap-4 justify-center pt-6 text-[14px]'>
-                    <span>Monthly</span>
+                    <span className={`${theme ==='dark'?'text-white':'text-dark-theme'}`}>Monthly</span>
                     <Switch
                     checked={isAnnual}
                     onChange={(checked)=>{setIsAnnual(checked)}}
                     />
-                    <span>Annually</span>
-                    <div className='uppercase text-straight-green bg-[#EDFAF3] font-normal text-[12px] px-3 py-1 rounded-full'>
+                    <span className={`${theme ==='dark'?'text-white':'text-dark-theme'}`}>Annually</span>
+                    <div className={`uppercase text-straight-green ${theme === 'dark'? 'bg-[#052E1F] text-white': 'text-straight-green bg-[#EDFAF3]'} font-normal text-[12px] px-3 py-1 rounded-full`}>
                         save 30%
                     </div>
                 </div>
@@ -52,18 +53,18 @@ export function PricingHeroSection({title='',subtitle='',pageLabel='',paymentOpt
                         : paymentOption.monthPayment;
                         return(
                             <li key={paymentOption.id}>
-                                <article className={`relative p-8 rounded-xl flex flex-col gap-2 bg-white shadow-xl ${paymentOption.badge && 'border-[3px] border-primary-blue'}`}>
-                                    {paymentOption.badge && <div className='bg-[#F4A016] py-2 px-3 rounded-full uppercase absolute left-1/2 -top-5 transform -translate-x-1/2'>{paymentOption.badge}</div>}
-                                    <h3 className='text-medium-gray uppercase text-[12px]'>{paymentOption.title}</h3>
-                                    {typeof paymentOption.monthPayment == 'number'? <p className='text-[16px] text-medium-gray'><span className='text-dark-theme font-bold text-[28px] font-header'>${formattedPrice}</span>/month</p>:<p className='text-dark-theme font-bold text-[28px] font-header capitalize'>{formattedPrice}</p>}
+                                <article className={`relative p-8 rounded-xl flex flex-col gap-2 ${theme === 'dark'?'bg-linear-to-br from-[#000000] to-[#331103] text-[#CBC9C2]':'bg-white text-medium-gray'} shadow-xl ${paymentOption.badge && 'border-[3px] border-primary-blue'}`}>
+                                    {paymentOption.badge && <div className='bg-[#F4A016] py-2 px-3 rounded-full uppercase text-dark-theme absolute left-1/2 -top-5 transform -translate-x-1/2'>{paymentOption.badge}</div>}
+                                    <h3 className='uppercase text-[12px]'>{paymentOption.title}</h3>
+                                    {typeof paymentOption.monthPayment == 'number'? <p className='text-[16px] '><span className={`${theme==='dark'?'text-white':'text-dark-theme'} font-bold text-[28px] font-header`}>${formattedPrice}</span>/month</p>:<p className={`${theme==='dark'?'text-white':'text-dark-theme'} font-bold text-[28px] font-header capitalize`}>{formattedPrice}</p>}
 
-                                    <p className='text-medium-gray text-[12px]'>{paymentOption.subtitle}</p>
+                                    <p className='text-[12px]'>{paymentOption.subtitle}</p>
                                     <ul className='flex flex-col gap-4 pb-6'>
                                         {paymentOption.privileges?.map((privilege,index)=>{
-                                            return(<li key={privilege}><p className='flex items-center gap-3.5 text-medium-gray text-[12px] pb-2 border-b border-b-gray-300'>{CircleCheckIcon && <CircleCheckIcon className='text-straight-green bg-green-200 rounded-full'/>} {privilege}</p></li>);
+                                            return(<li key={privilege}><p className={`flex items-center gap-3.5 text-[12px] pb-2 border-b ${theme === 'dark'?'border-b-gray-600':'border-b-gray-300'}`}>{CircleCheckIcon && <CircleCheckIcon className='text-straight-green bg-green-200 rounded-full'/>} {privilege}</p></li>);
                                         })}
                                     </ul>
-                                    <button className={`flex items-center justify-center gap-2 text-[12px] font-medium min-w-10 w-72 h-10 capitalize border border-gray-200 rounded-lg cursor-pointer transition-transform duration-100 ease-in-out ${paymentOption.badge && 'text-white bg-primary-blue'} hover:scale-110 hover:bg-gray-800 hover:text-white active:scale-95`}>{paymentOption.nextAction}<ArrowIcon className='text-sm'/></button>
+                                    <button className={`flex items-center justify-center gap-2 text-[12px] font-medium min-w-10 w-full h-10 capitalize border border-gray-200 rounded-lg cursor-pointer transition-transform duration-100 ease-in-out ${paymentOption.badge && 'text-white bg-primary-blue'} hover:scale-110 hover:bg-gray-800 hover:text-white active:scale-95`}>{paymentOption.nextAction}<ArrowIcon className='text-sm'/></button>
                                 </article>
                             </li>
                         );
--- src/components/PricingHeroSection/Switch.jsx
+++ src/components/PricingHeroSection/Switch.jsx
@@ -1,5 +1,7 @@
+import { useTheme } from "../../context/ThemeContext";
 
 export function Switch({ checked, onChange }) {
+  const {theme} = useTheme();
   return (
     <label className="inline-flex items-center gap-2.5 cursor-pointer">
       <input
@@ -9,8 +11,8 @@ export function Switch({ checked, onChange }) {
         className="hidden"
       />
       <div 
-      className={`w-12 h-[6.5] ${checked?'bg-blue-200':'bg-[#ccc]'} flex items-center border border-[#10399B] rounded-3xl relative transition-colors ease-in-out p-1 shadow-xl`}>
-        <div className={`w-4 h-4 rounded-full shadow transition-transform duration-200 ease-in-out ${checked?'bg-[#2563eb] translate-x-5.5':'bg-white'}`}></div>
+      className={`w-12 h-[6.5] ${theme === 'dark'?checked?'bg-[#081D5B]':'bg-white' :checked?'bg-blue-200':'bg-[#ccc]'} flex items-center border ${theme === 'dark'?'border-[#4783EB]':`border-[#10399B]`} rounded-3xl relative transition-colors ease-in-out p-1 shadow-xl`}>
+        <div className={`w-4 h-4 rounded-full shadow transition-transform duration-200 ease-in-out ${checked?`${theme==='dark'?'bg-[#75A2F0]':'bg-[#2563eb]'} translate-x-5.5`:`${theme==='dark'?'bg-[#ccc]':'bg-white'}`}`}></div>
       </div>
     </label>
   );
