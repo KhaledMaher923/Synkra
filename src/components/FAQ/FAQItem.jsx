532c9e093f8f730984fd16678d4ed1f49a6214b5
SohaibAlaraby <sohibalaraby9@gmail.com>
2026-09-13 15:45:22 +0300

    feat(faq): enhance code of faq
    - convert theme props in each sub component into calling theme context inside each subcomponent to enhance separation of concerns - convert faq data to props instead of importing it directly to enhance reusability

--- src/components/FAQ/FAQItem.jsx
+++ src/components/FAQ/FAQItem.jsx
@@ -1,14 +1,16 @@
 import { MdArrowBackIos } from "react-icons/md";
-export default function FAQItem({theme,title,description}){
+import { useTheme } from "../../context/ThemeContext";
 
+export default function FAQItem({question,answer}){
+    const {theme} = useTheme();
     return(
         <details className={`group border ${theme==='dark'?'border-gray-700':'border-gray-300'} rounded-xl open:shadow-md`}>
             <summary className={`flex justify-between items-center text-[18px] p-5 font-bold  list-none [::-webkit-details-marker]:hidden select-none rounded-t-xl ${theme==='dark'?'group-open:bg-[#1F1E1C]':'group-open:bg-neutral-stone'} max-lg:text-[16px]`}>
-                <span>{title}</span>
+                <span>{question}</span>
                 <MdArrowBackIos  className='w-6 -rotate-90 group-open:rotate-90'/>
                 </summary>
             <div className='p-5 bg-semi-white rounded-b-xl'>
-                <p className='text-medium-gray font-sans text-[16px] max-lg:text-[14px]'>{description}</p>
+                <p className='text-medium-gray font-sans text-[16px] max-lg:text-[14px]'>{answer}</p>
             </div>
         </details>
     );
--- src/components/FAQ/FAQSection.jsx
+++ src/components/FAQ/FAQSection.jsx
@@ -1,19 +1,18 @@
-import {FAQList,FAQTitle,FAQSubtitle} from '../../data/faqData.js';
 
 import FAQItem from "./FAQItem.jsx";
 import FAQHeader from "./FAQHeader.jsx";
 import {useTheme} from "../../context/ThemeContext.jsx";
 
-export default function FAQSection(){
+export function FAQSection({title,subtitle,QAList}){
     const {theme} = useTheme();
     return (
         <section className={`py-16 px-35 flex flex-col items-center gap-12 ${theme==='dark'? 'bg-dark-theme text-semi-white':'bg-light-theme text-dark-theme'} max-lg:py-6 max-lg:px-6`}>
-            <FAQHeader theme={theme} title={FAQTitle} subtitle={FAQSubtitle}/>
+            <FAQHeader title={title} subtitle={subtitle}/>
             <ul className='flex flex-col gap-5 w-full max-w-250'>
-                {FAQList.map((AQObj)=>{
+                {QAList.map((AQObj)=>{
                     return (
                         <li key={AQObj.id}>
-                            <FAQItem title={AQObj.question} description={AQObj.answer} theme={theme}/>
+                            <FAQItem {...AQObj}/>
                         </li>
                     );
                 })}
