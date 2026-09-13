2f0da079e5d6b4ffe10f722251a54ba266a8f3fb
Amira-Elsayed-Abdelaziz <amira.elsayed.abdelaziz@gmail.com>
2026-09-13 20:36:25 +0300

    adding background effects to stats section cards & fixing some responsive issues

--- src/components/StatsSection/StatsSection.jsx
+++ src/components/StatsSection/StatsSection.jsx
@@ -16,7 +16,7 @@ export default function StatsSection({page='default'}) {
   const { theme } = useTheme();
   return (
     <section
-      className={`p-[24px] relative md:pt-[40px] md:pb-[40px] md:pr-[64px] md:pl-[64px] md:max-w-[100%]
+      className={` p-[24px] relative lg:pt-[40px] lg:pb-[40px] lg:pr-[64px] lg:pl-[64px] lg:max-w-[100%]
     ${theme === "dark" ? "bg-dark-theme text-semi-white" : "bg-[#EBF2FD] text-dark-theme"} 
     `}
     >
@@ -24,7 +24,7 @@ export default function StatsSection({page='default'}) {
 
       <div className="pt-[24px] pb-[24px]  flex flex-col items-center gap-[24px]">
         <StatsSectionHeader title={SSTitle} subtitle={SSSubtitle} />
-        <div className="grid grid-cols-2  gap-[16px] w-full max-w-250 md:grid-cols-4 md:grid-rows-[repeat(3,minmax(400px,fit-content))] md: md:max-w-[100%]">
+        <div className="grid grid-cols-2  gap-[16px] w-full max-w-250 lg:grid-cols-4 lg:grid-rows-[repeat(3,minmax(400px,fit-content))] lg: lg:max-w-[100%]">
           {SSCardList.map((card) => {
             return (
               <StatsSectionCard key={card.id} info={card}></StatsSectionCard>
